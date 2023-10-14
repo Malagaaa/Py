239690aee7ae937b1606d2900c5c7306658a8404
Luisa <luisa-yi-qing.yang@student.tuiasi.ro>
2023-10-14 18:06:10 +0300

    Update

--- ex1.py
+++ ex1.py
@@ -1,1 +1,2 @@
 print("Documentați-vă de spre github și încercați crearea/descărcarea/actualizarea unor repository-uri.")
+print("Bonus (până la 0.5 puncte pentru cei care demonstrează la laborator în săpt a 2-a că pot gestiona un proiect pe github.")
